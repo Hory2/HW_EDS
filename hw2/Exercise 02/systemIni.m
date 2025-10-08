a941832be5145c0599ed28ef411ad0d974c17276
Duhor <dusanhorak01@seznam.cz>
2025-10-08 15:18:21 +0300

    plotingV1

--- hw2/Exercise 02/systemIni.m
+++ hw2/Exercise 02/systemIni.m
@@ -38,8 +38,7 @@ Tc = 10e-6;
 %% controller setting
 
 % desired voltage (pu)
-v_ref = 1.155 ;% to be added
-v_ref=1;
+v_ref = 2*sqrt(3) ;% to be added
 % angular frequency
 % omega = 2*pi()*f_rat ;% to be added
 omega=1;
