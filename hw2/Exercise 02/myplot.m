3f59485146e29107a3ca795a4f7367b60fcbc766
Duhor <dusanhorak01@seznam.cz>
2025-10-08 21:36:26 +0300

    finalHW2

--- hw2/Exercise 02/myplot.m
+++ hw2/Exercise 02/myplot.m
@@ -1,7 +1,34 @@
 close all
 figure
-stem(fft1.freq,fft1.mag/(Vdc*0.5),"filled",LineWidth=1,Marker="*",MarkerSize=1)
+stem(fft1.freq,fft1.mag,"filled",LineWidth=1,Marker="*",MarkerSize=1)
 xlabel('f[Hz]')
-ylabel('m')
+ylabel('u_{\alpha \beta}[p.u.]')
 
-saveas(gcf, ['fft1.png']);
+ saveas(gcf, ['fft1.2.png']);
+
+figure
+stem(fft2.freq,fft2.mag,"filled",LineWidth=1,Marker="*",MarkerSize=1)
+xlabel('f[Hz]')
+ylabel('u_{\alpha \beta}[p.u.]')
+ saveas(gcf, ['fft2.1.png']);
+
+figure
+stem(fft3.freq,fft3.mag,"filled",LineWidth=1,Marker="*",MarkerSize=1)
+xlabel('f[Hz]')
+ylabel('u_{\alpha \beta}[p.u.]')
+ saveas(gcf, ['fft3.1.png']);
+
+%%
+figure
+hold on
+stem(fft1.freq,fft1.mag,"filled",LineWidth=1,Marker="*",MarkerSize=1)
+
+stem(fft4.freq,fft4.mag,"filled",LineWidth=1,Marker="*",MarkerSize=1)
+
+    
+
+legend('Natural sampling', 'Asymmetric regular sampling')
+xlabel('f[Hz]')
+ylabel('u_{\alpha \beta}[p.u.]')
+saveas(gcf, ['fft4.1.png']);
+
